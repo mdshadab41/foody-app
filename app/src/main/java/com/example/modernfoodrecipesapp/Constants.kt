6de47fd82661b9a13ce3c21cae9b7f42c02d38ce
Shadab <shadabjhu00447@gmail.com>
2023-09-27 15:25:35 +0530

    Commit - 1 - StartWithRetrofit

---
+++ app/src/main/java/com/example/modernfoodrecipesapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.modernfoodrecipesapp
+
+class Constants {
+
+    companion object{
+        const val BASE_URL =  "https://api.spoonacular.com"
+        const val API_KEY = "e7cb80d40a0d426388f8857f72dfa716"
+
+    }
+}
--- app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/RecipesFragment.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/RecipesFragment.kt
@@ -1,10 +1,11 @@
-package com.example.modernfoodrecipesapp
+package com.example.modernfoodrecipesapp.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.modernfoodrecipesapp.R
 
 
 class RecipesFragment : Fragment() {
